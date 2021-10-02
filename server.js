ced30f91cff165a2d1d85d000c6226f9b0f0af88
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2021-10-02 15:48:27 +0530

    changes

--- server.js
+++ server.js
@@ -56,5 +56,8 @@ app.post('/refresh', (req, res) => {
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('client/build'));
 }
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 app.listen(PORT, () => console.log('listening on port 3001'));
