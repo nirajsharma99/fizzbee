ca7e31f598d5e884a8263c6a6cd28d71440f14b2
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2021-11-21 23:04:44 +0530

    changes

--- client/src/components/player/maxplayertypes/player2.js
+++ client/src/components/player/maxplayertypes/player2.js
@@ -85,7 +85,7 @@ function MaxPlayer2({
       <div className="mp2-controls">
         <div className="music-info">
           <div className="s-info">
-            <div>
+            <div className="s-info-text">
               <span className="np-name">
                 {current ? current.name : 'Music track'}
               </span>
