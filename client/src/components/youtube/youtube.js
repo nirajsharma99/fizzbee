a0ad7f2f23b59efdd9c77b066f031b897e17219f
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2021-12-05 00:13:05 +0530

    fix

--- client/src/components/youtube/youtube.js
+++ client/src/components/youtube/youtube.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { REACT_APP_YT_KEY } = process.env;
-console.log(REACT_APP_YT_KEY);
+
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
