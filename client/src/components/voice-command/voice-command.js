5add41bd0c202800432913c60a0ea618a235709a
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2022-01-27 09:26:06 +0530

    changes

--- client/src/components/voice-command/voice-command.js
+++ client/src/components/voice-command/voice-command.js
@@ -117,14 +117,14 @@ function VC() {
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress);
     window.addEventListener('keyup', handleKeyUp);
-    window.addEventListener('pointerdown', handleKeyPress);
-    window.addEventListener('pointerup', handleKeyUp);
+    window.addEventListener('touchstart', handleKeyPress);
+    window.addEventListener('touchend', handleKeyUp);
 
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
       window.removeEventListener('keyup', handleKeyUp);
-      window.removeEventListener('pointerdown', handleKeyPress);
-      window.removeEventListener('pointerup', handleKeyUp);
+      window.removeEventListener('touchstart', handleKeyPress);
+      window.removeEventListener('touchend', handleKeyUp);
     };
   }, [handleKeyPress]);
 
