fa1e17216e1a11b8d2a95d904e52d6b2d8950a5b
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2022-12-08 10:24:01 +0530

    changed message

--- client/src/components/windowSize.js
+++ client/src/components/windowSize.js
@@ -22,7 +22,8 @@ function Suggestion() {
       <div>
         <p>
           For better UI experience in Smartphones, Open options and click on{' '}
-          <i>"Add to Home Screen" or You can just install the app from the prompt shown above.</i>
+          <i>"Add to Home Screen"</i>
+          or You can just install the app from the prompt shown above.
           <br />
           Then play through App Shortcut :)
         </p>
