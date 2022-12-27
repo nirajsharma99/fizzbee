8901f0609ad0ded2a96be9e888c81b7aed7bf8bb
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2022-12-27 23:33:35 +0530

    changes

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const io = new Server(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
-    transports: ['websocket']
-  }
+  },
+  transports: ['websocket']
 })
 /*const server = http.createServer(app);
 const io = socket(server, {
