065effb78d115d91bfa7992d222aa4e29f86fb1d
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2022-12-27 21:38:54 +0530

    changes

--- client/src/components/player/party/party.js
+++ client/src/components/player/party/party.js
@@ -27,12 +27,10 @@ function Party() {
     const API_ENDPOINT = REACT_APP_API_ENDPOINT || '';
     const ENDPOINT = REACT_APP_ENDPOINT || 'http://localhost:3000';
 
-    socket = io(API_ENDPOINT, {
-        transports: ['websocket']
-    });
 
     useEffect(() => {
         if (!user?.id || !token) return;
+        socket = io(API_ENDPOINT);
         socket.emit('getParty', { userId: user.id, username: user.display_name, partyOn: partyMode, token: token });
         socket.on('receiveParty', (data) => {
             if (data) {
@@ -44,6 +42,7 @@ function Party() {
 
     useEffect(() => {
         if (data?.votingId) {
+            socket = io(API_ENDPOINT);
             socket.emit('getPartyDetails', { votingId: data.votingId });
             socket.on('receivePartyDetails', (data) => {
                 if (data) {
